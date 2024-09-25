3677de0dc87dd3b6143a5515fa1e2d1c8babacce
ahmadalsajid <ahmadalsajid@gmail.com>
2024-09-26 00:56:53 +0600

    minor updatesa

--- create-superuser.sh
+++ create-superuser.sh
@@ -18,10 +18,10 @@ fi
 python manage.py makemigrations
 python manage.py migrate --noinput
 
-#if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-#  python manage.py ensure_adminuser --username="$DJANGO_SUPERUSER_USERNAME" \
-#    --email="$DJANGO_SUPERUSER_EMAIL" \
-#    --password="$DJANGO_SUPERUSER_PASSWORD"
-#fi
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py ensure_adminuser --username="$DJANGO_SUPERUSER_USERNAME" \
+    --email="$DJANGO_SUPERUSER_EMAIL" \
+    --password="$DJANGO_SUPERUSER_PASSWORD"
+fi
 
 "$@"
